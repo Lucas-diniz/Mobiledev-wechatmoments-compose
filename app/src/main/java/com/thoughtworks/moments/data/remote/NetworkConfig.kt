cd33f05ae59415890916b6ca851e8f21b85af2f7
lucasdiniz <lukas-diniz@hotmail.com>
2025-10-28 21:32:41 -0300

    feat: Refactor repository to use interface

---
+++ app/src/main/java/com/thoughtworks/moments/data/remote/NetworkConfig.kt
@@ -0,0 +1,5 @@
+package com.thoughtworks.moments.data.remote
+
+object NetworkConfig {
+    const val BASE_URL = "http://10.0.2.2:2727/"
+}
--- app/src/main/java/com/thoughtworks/moments/di/dataDi.kt
+++ app/src/main/java/com/thoughtworks/moments/di/dataDi.kt
@@ -1,9 +1,32 @@
 package com.thoughtworks.moments.di
 
+import com.thoughtworks.moments.data.remote.NetworkConfig.BASE_URL
 import com.thoughtworks.moments.data.remote.repository.MomentRepositoryDefault
+import com.thoughtworks.moments.data.remote.service.MomentService
 import com.thoughtworks.moments.domain.repository.MomentRepository
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 val dataDi = module {
-    single<MomentRepository> { MomentRepositoryDefault() }
+
+    single<MomentRepository> { MomentRepositoryDefault(get<MomentService>()) }
+
+    single { get<Retrofit>().create(MomentService::class.java) }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().also {
+                        it.setLevel(HttpLoggingInterceptor.Level.BASIC)
+                    })
+                    .build()
+            )
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
 }
